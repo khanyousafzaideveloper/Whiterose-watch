dce81d22eab120bf90d8d6573c460e26751367c9
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2022-10-13 22:11:09 +0500

    threads: but does not work with sleep now

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import time
 from playsound import playsound
+import threading
+
+def beep():
+    file_name = 'wrist_watch_beep.mp3'
+    return file_name
+
+def curr_time():
+    print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec}")
+
+t1 = threading.Thread(target=beep)
+t2 = threading.Thread(target=curr_time)
+
+t1.start()
+t2.start()
+
+t1.join()
+t1.join()
 
 while True:
     t = time.gmtime()
     sec = str(t.tm_sec)
     if(sec=='0'):
-        playsound('wrist_watch_beep.mp3')
-        print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec}")
+        playsound(beep())
+        curr_time()
         time.sleep(57)
      
+
